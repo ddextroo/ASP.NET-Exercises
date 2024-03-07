522ac6dbddbf5bc99597cd12b506fb958f8fe60b
Dexter Inguito <37497130+ddextroo@users.noreply.github.com>
2024-03-07 21:03:54 +0800

    Update HomeController.cs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -26,6 +26,40 @@ namespace MyExercises.Controllers
         {
             return View();
         }
+        public ActionResult Accounting()
+        {
+            ViewBag.Message = "Your Accounting page.";
+
+            return View();
+        }
+        public ActionResult StudentEntry()
+        {
+            var data = new List<object>();
+            var tuition_units = float.Parse(Request["subject"]) * 3;
+            var total_fee = float.Parse(tuition_units + Request["labfee"] + Request["registration"] + Request["tuition"] + Request["misc"]);
+            data.Add(new
+            {
+                idnum = Request["fname"],
+                lname = Request["lname"],
+                fname = Request["fname"],
+                gender = Request["gender"],
+                course_code = Request["course_code"],
+                course = Request["course"],
+                year = Request["year"],
+                subject = Request["subject"],
+                registration = Request["registration"],
+                tuition = Request["tuition"],
+                labfee = Request["labfee"],
+                misc = Request["misc"],
+                tuition_unit = tuition_units,
+                total_fee = total_fee,
+                prelim = 0,
+                midterm = 0,
+                semifinal = 0,
+                final = total_fee
+            });
+            return Json(data, JsonRequestBehavior.AllowGet);
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
