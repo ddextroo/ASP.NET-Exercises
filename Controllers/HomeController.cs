4a82dce6bd5760751f787fb938c72e2324167be3
Dexter Inguito <37497130+ddextroo@users.noreply.github.com>
2024-03-14 20:52:02 +0800

    Update HomeController.cs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -32,36 +32,49 @@ namespace MyExercises.Controllers
 
             return View();
         }
-        public ActionResult StudentEntry()
-        {
-            var data = new List<object>();
-            var total_units = float.Parse(Request.Form["subject"]) * 3;
-            var tuition_tuition = float.Parse(Request.Form["tuition"]) * total_units;
-            var total_f = float.Parse(Request.Form["labfee"]) + float.Parse(Request.Form["registration"]) + float.Parse(Request.Form["tuition"]) + float.Parse(Request.Form["misc"]) + tuition_tuition;
-            data.Add(new
-            {
-                idnum = Request.Form["idnum"].ToString(),
-                lname = Request.Form["lname"].ToString(),
-                fname = Request.Form["fname"].ToString(),
-                gender = Request.Form["gender"].ToString(),
-                course_code = Request.Form["course_code"].ToString(),
-                course = Request.Form["course"].ToString(),
-                year = Request.Form["year"].ToString(),
-                subject = Request.Form["subject"].ToString(),
-                registration = Request.Form["registration"].ToString(),
-                tuition = Request.Form["tuition"].ToString(),
-                labfee = Request.Form["labfee"].ToString(),
-                misc = Request.Form["misc"].ToString(),
-                total_tuition_units = tuition_tuition,
-                total_fee = total_f,
-                prelim = total_f * 0.53,
-                midterm = total_f * 0.64,
-                semifinal = total_f * 0.75,
-                final = total_f,
-                mode_of_payment = (total_f >= 8000) ? "Cash" : (total_f >= 5000) ? "Check" : "Credit"
-            });
-            return Json(data);
-        }
+        [HttpPost]
+public ActionResult StudentEntry()
+{
+    var data = new List<object>();
+    var total_units = float.Parse(Request.Form["subject"]) * 3;
+    var tuition_tuition = float.Parse(Request.Form["tuition"]) * total_units;
+    var total_f = float.Parse(Request.Form["labfee"]) + float.Parse(Request.Form["registration"]) + float.Parse(Request.Form["tuition"]) + float.Parse(Request.Form["misc"]) + tuition_tuition;
+    data.Add(new
+    {
+        idnum = Request.Form["idnum"].ToString(),
+        lname = Request.Form["lname"].ToString(),
+        fname = Request.Form["fname"].ToString(),
+        gender = Request.Form["gender"].ToString(),
+        course_code = Request.Form["course_code"].ToString(),
+        course = Request.Form["course"].ToString(),
+        year = Request.Form["year"].ToString(),
+        subject = Request.Form["subject"].ToString(),
+        registration = Request.Form["registration"].ToString(),
+        tuition = Request.Form["tuition"].ToString(),
+        labfee = Request.Form["labfee"].ToString(),
+        misc = Request.Form["misc"].ToString(),
+        total_tuition_units = tuition_tuition,
+        total_fee = total_f,
+        prelim = total_f * 0.53,
+        midterm = total_f * 0.64,
+        semifinal = total_f * 0.75,
+        final = total_f,
+        check = float.Parse(Request.Form["amount_tendered"]) - 2,
+        status = Request.Form["status"],
+        mode_of_payment = (total_f >= 8000) ? "Cash" : (total_f >= 5000) ? "Check" : "Credit"
+    });
+    return Json(data);
+}
+[HttpGet]
+public ActionResult StudentEntry(string idnum)
+{
+    var data = new List<object>();
+    data.Add(new
+    {
+        idnum = idnum,
+    });
+    return Json(data);
+}
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
