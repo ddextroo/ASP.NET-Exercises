11ce8ee444c8bdf76aa35bcad1b2ce044d2c62d7
Dexter Inguito <37497130+ddextroo@users.noreply.github.com>
2024-04-11 13:31:59 +0800

    Update student_accounting.js

--- wwwroot/js/student_accounting.js
+++ wwwroot/js/student_accounting.js
@@ -1,7 +1,7 @@
 // Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
+// Write your JavaScript code.s
 
 $(document).ready(function () {
     var studentModel = {
